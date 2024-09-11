b076c9bef427de4c52bf6297374ac8325729852d
Michael Innocenzi <mikeinnojr@gmail.com>
2024-09-10 20:22:29 -0400

    added labs component from class

---
+++ src/Labs.tsx
@@ -0,0 +1,17 @@
+const Labs = () => {
+    return (
+        <div>
+        <h2>Lab 1</h2>
+        <h3>Exercise 1.1</h3>
+        <h3>Exercise 1.2</h3>
+        <h2>Lab 2</h2>
+        <h3>Exercise 2.1</h3>
+        <h3>Exercise 2.2</h3>
+        <h2>Lab 3</h2>
+        <h3>Exercise 3.1</h3>
+        <h3>Exercise 3.2</h3>
+      </div>
+    );
+}
+
+export default Labs;
