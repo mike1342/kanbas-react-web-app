118e76e6b5ceb4c60a77bb337fec6424b9ab56cb
Caroline Pham <carolinetpham@outlook.com>
2024-12-04 15:32:01 -0500

    fixing div title and adding modules

--- src/Kanbas/Courses/Quizzes/index.tsx
+++ src/Kanbas/Courses/Quizzes/index.tsx
@@ -3,6 +3,8 @@ import { BsThreeDotsVertical } from "react-icons/bs";
 import { FaPlus } from "react-icons/fa6";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router";
+import { IoMdArrowDropdown } from "react-icons/io";
+import QuizzesControlButtons from "./QuizzesControlButtons";
 
 export default function Quizzes() {
   const { cid } = useParams();
@@ -24,54 +26,71 @@ export default function Quizzes() {
 
   return (
     <div id="wd-quizzes">
-      <div className="row mb-5">
-        <div id="wd-search-quizzes-container" className="col">
-          <input
-            id="wd-search-quizzes"
-            placeholder="Search for Quiz"
-            className="form-control"
-          />
-        </div>
-        {currentUser.role === "FACULTY" && (
-          <div className="col">
-            <button
-              id="wd-quiz-content-menu"
-              className="btn btn-md btn-secondary me-1 text-black float-end"
-              onClick={toggleMenu}
-            >
-              <BsThreeDotsVertical />
-            </button>
-            {menuVisible && (
-              <div className="dropdown-menu show position-absolute end-0 mt-5">
-                <button className="dropdown-item" onClick={handleEdit}>
-                  Edit
-                </button>
-                <button
-                  className="dropdown-item text-danger"
-                  onClick={handleDelete}
-                >
-                  Delete
-                </button>
-                <button className="dropdown-item" onClick={handlePublishToggle}>
-                  {isPublished ? "Unpublish" : "Publish"}
-                </button>
-              </div>
-            )}
-            <a
-              id="wd-add-quiz"
-              className="btn btn-md btn-danger me-1 float-end"
-              href={`#/Kanbas/Courses/${cid}/Quizes/NewQuiz`}
-            >
-              <FaPlus
-                className="position-relative me-2"
-                style={{ bottom: "1px" }}
-              />
-              Quiz
-            </a>
+      <div id="wd-quiz-options">
+        <div className="row mb-5">
+          <div id="wd-search-quizzes-container" className="col">
+            <input
+              id="wd-search-quizzes"
+              placeholder="Search for Quiz"
+              className="form-control"
+            />
           </div>
-        )}
+          {currentUser.role === "FACULTY" && (
+            <div className="col">
+              <button
+                id="wd-quiz-context-menu"
+                className="btn btn-md btn-secondary me-1 text-black float-end"
+                onClick={toggleMenu}
+              >
+                <BsThreeDotsVertical />
+              </button>
+              {menuVisible && (
+                <div className="dropdown-menu show position-absolute end-0 mt-5">
+                  <button className="dropdown-item" onClick={handleEdit}>
+                    Edit
+                  </button>
+                  <button
+                    className="dropdown-item text-danger"
+                    onClick={handleDelete}
+                  >
+                    Delete
+                  </button>
+                  <button
+                    className="dropdown-item"
+                    onClick={handlePublishToggle}
+                  >
+                    {isPublished ? "Unpublish" : "Publish"}
+                  </button>
+                </div>
+              )}
+              <a
+                id="wd-add-quiz"
+                className="btn btn-md btn-danger me-1 float-end"
+                href={`#/Kanbas/Courses/${cid}/Quizes/NewQuiz`}
+              >
+                <FaPlus
+                  className="position-relative me-2"
+                  style={{ bottom: "1px" }}
+                />
+                Quiz
+              </a>
+            </div>
+          )}
+        </div>
+        <hr />
+        <br />
+        <div id="wd-quiz-modules">
+          <ul id="wd-quizzes-list" className="list-group rounded-0">
+            <li className="wd-quiz list-group-item p-0 mb-5 fs-5 btn-secondary">
+              <div className="wd-title border p-3 ps-2">
+                <IoMdArrowDropdown className="me-2 fs-3" />
+                <span className="fw-bold">Assignment Quizzes</span>
+                {currentUser.role === "FACULTY" && <QuizzesControlButtons />}
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <hr />
     </div>
   );
 }
