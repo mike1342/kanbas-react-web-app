fb11fa5385ff7cf3c5c8fe052c1418f510129172
Caroline Pham <carolinetpham@outlook.com>
2024-12-04 13:56:55 -0500

    creating quiz control buttons and adding drop down for content menu

--- src/Kanbas/Courses/Quizzes/index.tsx
+++ src/Kanbas/Courses/Quizzes/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { FaPlus } from "react-icons/fa6";
 import { useSelector } from "react-redux";
@@ -6,6 +7,22 @@ import { useParams } from "react-router";
 export default function Quizzes() {
   const { cid } = useParams();
   const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [isPublished, setIsPublished] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  const handleEdit = () => {};
+
+  const handleDelete = () => {};
+
+  const handlePublishToggle = () => {
+    setIsPublished(!isPublished);
+    console.log(isPublished ? "Quiz unpublished." : "Quiz published.");
+  };
+
   return (
     <div id="wd-quizzes">
       <div className="row mb-5">
@@ -19,11 +36,28 @@ export default function Quizzes() {
         {currentUser.role === "FACULTY" && (
           <div className="col">
             <button
-              id="wd-quiz-group"
+              id="wd-quiz-content-menu"
               className="btn btn-md btn-secondary me-1 text-black float-end"
+              onClick={toggleMenu}
             >
               <BsThreeDotsVertical />
             </button>
+            {menuVisible && (
+              <div className="dropdown-menu show position-absolute end-0 mt-5">
+                <button className="dropdown-item" onClick={handleEdit}>
+                  Edit
+                </button>
+                <button
+                  className="dropdown-item text-danger"
+                  onClick={handleDelete}
+                >
+                  Delete
+                </button>
+                <button className="dropdown-item" onClick={handlePublishToggle}>
+                  {isPublished ? "Unpublish" : "Publish"}
+                </button>
+              </div>
+            )}
             <a
               id="wd-add-quiz"
               className="btn btn-md btn-danger me-1 float-end"
